b776c848c774f4e4451698b7b5b62525ab0c7a51
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-11-11 15:58:49 +0300

    Create Categories functions

--- Helpers/RouteResolutionHelper.cs
+++ Helpers/RouteResolutionHelper.cs
@@ -1,23 +1,40 @@
-﻿using ArpellaStores.Services;
-using ArpellaStores.Models;
+﻿using ArpellaStores.Models;
+using ArpellaStores.Services;
 
 namespace ArpellaStores.Helpers
 {
     public class RouteResolutionHelper : IRouteResolutionHelper
     {
+        private readonly ICategoriesService _categoriesService;
+        private readonly ISubcategoriesServices _subcategoriesServices;
         private readonly IProductManagement _productManagement;
-        public RouteResolutionHelper(IProductManagement productManagement)
+        public RouteResolutionHelper(ICategoriesService categoriesService, ISubcategoriesServices subcategoriesServices, IProductManagement productManagement)
         {
+            _categoriesService = categoriesService;
+            _subcategoriesServices = subcategoriesServices;
             _productManagement = productManagement;
         }
         public void addMappings(WebApplication app)
         {
             app.MapGet("/", () => "Hello World!");
-            app.MapGet("/products", () => this._productManagement.GetProducts());
-            app.MapGet("/products/{id}", (string id) => this._productManagement.GetProduct(id));
-            app.MapPost("/products", (Product product) => this._productManagement.CreateProduct(product));
-            app.MapPut("/products/{id}", (Product product, string id) => this._productManagement.UpdateProductDetails(product, id));
-            app.MapDelete("/products/{id}", (string id) => this._productManagement.RemoveProduct(id));
+            // Products Routes
+            //app.MapGet("/products", () => this._productManagement.GetProducts());
+            //app.MapGet("/products/{id}", (string id) => this._productManagement.GetProduct(id));
+            //app.MapPost("/products", (Product product) => this._productManagement.CreateProduct(product));
+            //app.MapPut("/products/{id}", (Product product, string id) => this._productManagement.UpdateProductDetails(product, id));
+            //app.MapDelete("/products/{id}", (string id) => this._productManagement.RemoveProduct(id));
+
+            // Categories Routes
+            app.MapGet("/categories", ()=> this._categoriesService.GetCategories()).WithTags("Categories").Produces(200).Produces(404).Produces<List<Category>>();
+            app.MapGet("/categories/{id}", (string id) => this._categoriesService.GetCategory(id)).WithTags("Categories").Produces(200).Produces(404).Produces<Category>();
+            app.MapPost("/categories", (Category category) => this._categoriesService.CreateCategory(category)).WithTags("Categories").Produces<Category>();
+            app.MapPut("/categories/{id}", (Category category, string id) => this._categoriesService.UpdateCategoryDetails(category, id)).WithTags("Categories").Produces<Category>();
+            app.MapDelete("/categories/{id}", (string id) => this._categoriesService.RemoveCategory(id)).WithTags("Categories").Produces(200).Produces(404).Produces<Category>();
+
+            // Subcategories Routes
+            //app.MapGet("/subcategories", ()  =>  this._subcategoriesServices.GetSubcategories());
+            //app.MapPost("/subcategories", (Subcategory subcategory) => this._subcategoriesServices.CreateSubcategory(subcategory));
+            //app.MapDelete("/subcategories/{id}", (string id)=> this._subcategoriesServices.RemoveSubcategory(id));
         }
     }
 }
---
+++ Services/SubcategoriesService.cs
@@ -0,0 +1,41 @@
+﻿using ArpellaStores.Data;
+using ArpellaStores.Models;
+
+namespace ArpellaStores.Services
+{
+    public class SubcategoriesService : ISubcategoriesServices
+    {
+        private readonly ArpellaContext _context;
+        public SubcategoriesService(ArpellaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Subcategory>> GetSubcategories()
+        {
+            var subcategories = _context.Subcategories.ToList();
+            return subcategories;
+        }
+        public Subcategory CreateSubcategory(Subcategory subcategory)
+        {
+            var newSubcategory = new Subcategory
+            {
+                Id = subcategory.Id,
+                SubcategoryName = subcategory.SubcategoryName,
+                CategoryId = subcategory.CategoryId
+            };
+            _context.Add(newSubcategory);
+            _context.SaveChangesAsync();
+            return newSubcategory;
+        }
+        public Subcategory? RemoveSubcategory(string id)
+        {
+            var retrievedSubCategory = _context.Subcategories.FirstOrDefault(c => c.Id == id);
+            if (retrievedSubCategory != null)
+            {
+                _context.Remove(retrievedSubCategory);
+                _context.SaveChangesAsync();
+            };
+            return retrievedSubCategory;
+        }
+    }
+}
