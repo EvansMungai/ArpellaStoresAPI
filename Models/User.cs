702ffc9462b2d724f18441a60c41de5c7e4a2b31
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-02-18 11:16:31 +0300

    Added lastlogin feature.

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,6 @@ public partial class User : IdentityUser
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public DateTime LastLoginTime { get; set; }
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
