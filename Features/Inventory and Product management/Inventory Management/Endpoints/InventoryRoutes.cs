81c4e1910861229dc1dbd6f83dd0906ef4b08559
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-05-25 14:08:12 +0300

    Change datatype in inventory management.

--- Features/Inventory and Product management/Inventory Management/Endpoints/InventoryRoutes.cs
+++ Features/Inventory and Product management/Inventory Management/Endpoints/InventoryRoutes.cs
@@ -20,11 +20,11 @@ public class InventoryRoutes : IRouteRegistrar
     {
         var app = webApplication.MapGroup("").WithTags("Inventories");
         app.MapGet("/inventories", () => this._inventoryService.GetInventories()).Produces(200).Produces(404).Produces<List<Inventory>>();
-        app.MapGet("/inventory/{id}", (int id) => this._inventoryService.GetInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
+        app.MapGet("/inventory/{id}", (string id) => this._inventoryService.GetInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
         app.MapPost("/inventory", (Inventory inventory) => this._inventoryService.CreateInventory(inventory)).Produces(200).Produces(404).Produces<Inventory>();
         app.MapPost("/inventories", (IFormFile file) => this._inventoryService.CreateInventories(file)).Produces(200).Produces(404).Produces<List<Inventory>>().DisableAntiforgery();
-        app.MapPut("/inventory/{id}", (Inventory inventory, int id) => this._inventoryService.UpdateInventory(inventory, id)).Produces(200).Produces(404).Produces<Inventory>();
-        app.MapDelete("/inventory/{id}", (int id) => this._inventoryService.RemoveInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
+        app.MapPut("/inventory/{id}", (Inventory inventory, string id) => this._inventoryService.UpdateInventory(inventory, id)).Produces(200).Produces(404).Produces<Inventory>();
+        app.MapDelete("/inventory/{id}", (string id) => this._inventoryService.RemoveInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
         app.MapGet("/inventorylevels", () => this._inventoryService.CheckInventoryLevels()).Produces(200).Produces(404).Produces<List<Inventory>>();
     }
 }
