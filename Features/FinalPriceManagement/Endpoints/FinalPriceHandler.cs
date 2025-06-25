dd722f498651e7d690f7aa476e01a871fd49d404
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-06-26 00:58:25 +0300

    Fix final price management services and endpoints.

---
+++ Features/FinalPriceManagement/Endpoints/FinalPriceHandler.cs
@@ -0,0 +1,45 @@
+﻿using ArpellaStores.Extensions;
+using ArpellaStores.Features.FinalPriceManagement.Models;
+using ArpellaStores.Features.FinalPriceManagement.Services;
+
+namespace ArpellaStores.Features.FinalPriceManagement.Endpoints;
+
+public class FinalPriceHandler : IHandler
+{
+    public static string RouteName => "Final Price Management";
+    private readonly IFinalPriceService _finalPriceService;
+    private readonly ICouponService _couponService;
+    private readonly IDiscountService _discountService;
+    private readonly IFlashsaleService _flashsaleService;
+    public FinalPriceHandler(IFinalPriceService finalPriceService, ICouponService couponService, IDiscountService discountService, IFlashsaleService flashsaleService)
+    {
+        _finalPriceService = finalPriceService;
+        _couponService = couponService;
+        _discountService = discountService;
+        _flashsaleService = flashsaleService;
+    }
+
+    #region Final Price Handler 
+    public Task<decimal> GetFinalPriceAsync(int productId, string couponCode) => _finalPriceService.GetFinalPriceAsync(productId, couponCode);
+    #endregion
+
+    #region Coupons Handler
+    public Task<IResult> GetCoupons() => _couponService.GetCoupons();
+    public Task<IResult> GetCoupon(int couponId) => _couponService.GetCoupon(couponId);
+    public Task<IResult> CreateCoupon(Coupon coupon) => _couponService.CreateCoupon(coupon);
+    public Task<IResult> UpdateCoupon(Coupon update, int id) => _couponService.UpdateCoupon(update, id);
+    public Task<IResult> RemoveCoupon(int id) => _couponService.RemoveCoupon(id);
+    #endregion
+
+    #region Discount Handler
+    public Task<IResult> GetFinalPrice(int productId, string couponCode = null) => _discountService.GetFinalPrice(productId, couponCode);
+    #endregion
+
+    #region Flashsale Handler
+    public Task<IResult> GetFlashSales()=> _flashsaleService.GetFlashSales();
+    public Task<IResult> GetFlashSale(int flashSaleId) => _flashsaleService.GetFlashSale(flashSaleId);
+    public Task<IResult> CreateFlashSale(Flashsale flashsale) => _flashsaleService.CreateFlashSale(flashsale);
+    public Task<IResult> UpdateFlashSale(Flashsale update, int id) => _flashsaleService.UpdateFlashSale(update, id);
+    public Task<IResult> RemoveFlashSale(int id) => _flashsaleService.RemoveFlashsale(id);
+    #endregion
+}
--- Features/FinalPriceManagement/Endpoints/FinalPriceRoutes.cs
+++ Features/FinalPriceManagement/Endpoints/FinalPriceRoutes.cs
@@ -1,48 +1,43 @@
 ﻿using ArpellaStores.Extensions;
 using ArpellaStores.Features.FinalPriceManagement.Models;
-using ArpellaStores.Features.FinalPriceManagement.Services;
 
 namespace ArpellaStores.Features.FinalPriceManagement.Endpoints;
 
 public class FinalPriceRoutes : IRouteRegistrar
 {
-    private readonly IFinalPriceService _finalPriceService;
-    private readonly ICouponService _couponService;
-    private readonly IFlashsaleService _flashsaleService;
-    public FinalPriceRoutes(IFinalPriceService finalPriceService, ICouponService couponService, IFlashsaleService flashsaleService)
-    {
-        _finalPriceService = finalPriceService;
-        _couponService = couponService;
-        _flashsaleService = flashsaleService;   
-    }
-
     public void RegisterRoutes(WebApplication app)
     {
         MapFinalPriceRoutes(app);
         MapCouponRoutes(app);
+        MapDiscountRoutes(app);
         MapFlashsaleRoutes(app);
     }
 
     public void MapFinalPriceRoutes(WebApplication webApplication)
     {
-        webApplication.MapGet("/final-price", (int productId, string? couponCode) => _finalPriceService.GetFinalPriceAsync(productId, couponCode));
+        webApplication.MapGet("/final-price", (FinalPriceHandler handler, int productId, string? couponCode) => handler.GetFinalPriceAsync(productId, couponCode)).WithTags("Final-Price");
     }
     public void MapCouponRoutes(WebApplication webApplication)
     {
         var app = webApplication.MapGroup("").WithTags("Coupon");
-        app.MapGet("/coupons", () => this._couponService.GetCoupons()).Produces(200).Produces(404).Produces<List<Coupon>>();
-        app.MapGet("/coupon/{id}", (int id) => this._couponService.GetCoupon(id)).Produces(200).Produces(404).Produces<Coupon>();
-        app.MapPost("/coupon", (Coupon coupon) => this._couponService.CreateCoupon(coupon)).Produces(200).Produces(500).Produces<Coupon>();
-        app.MapPut("/coupon/{id}", (Coupon coupon, int id) => this._couponService.UpdateCoupon(coupon, id)).Produces(200).Produces(404).Produces(500).Produces<Coupon>();
-        app.MapDelete("/coupon/{id}", (int id) => this._couponService.RemoveCoupon(id)).Produces(200).Produces(404).Produces(500).Produces<Coupon>();
+        app.MapGet("/coupons", (FinalPriceHandler handler) => handler.GetCoupons()).Produces(200).Produces(404).Produces<List<Coupon>>();
+        app.MapGet("/coupon/{id}", (FinalPriceHandler handler, int id) => handler.GetCoupon(id)).Produces(200).Produces(404).Produces<Coupon>();
+        app.MapPost("/coupon", (FinalPriceHandler handler, Coupon coupon) => handler.CreateCoupon(coupon)).Produces(200).Produces(500).Produces<Coupon>();
+        app.MapPut("/coupon/{id}", (FinalPriceHandler handler, Coupon coupon, int id) => handler.UpdateCoupon(coupon, id)).Produces(200).Produces(404).Produces(500).Produces<Coupon>();
+        app.MapDelete("/coupon/{id}", (FinalPriceHandler handler, int id) => handler.RemoveCoupon(id)).Produces(200).Produces(404).Produces(500).Produces<Coupon>();
+    }
+    public void MapDiscountRoutes(WebApplication webApplication)
+    {
+        var app = webApplication.MapGroup("").WithTags("Discount");
+        app.MapGet("/discounts", (FinalPriceHandler handler, int productId, string couponCode) => handler.GetFinalPrice(productId, couponCode)).Produces(200).Produces(404).Produces<Discount>();
     }
     public void MapFlashsaleRoutes(WebApplication webApplication)
     {
         var app = webApplication.MapGroup("").WithTags("Flashsales");
-        app.MapGet("/flashsales", () => this._flashsaleService.GetFlashSales()).Produces(200).Produces(404).Produces<List<Flashsale>>();
-        app.MapGet("/flashsale/{id}", (int id) => this._flashsaleService.GetFlashSale(id)).Produces(200).Produces(404).Produces<Flashsale>();
-        app.MapPost("/flashsale", (Flashsale flashsale) => this._flashsaleService.CreateFlashSale(flashsale)).Produces(200).Produces(500).Produces<Flashsale>();
-        app.MapPut("/flashsale/{id}", (Flashsale flashsale, int id) => this._flashsaleService.UpdateFlashSale(flashsale, id)).Produces(200).Produces(404).Produces(500).Produces<Flashsale>();
-        app.MapDelete("/flashsale/{id}", (int id) => this._flashsaleService.RemoveFlashsale(id)).Produces(200).Produces(404).Produces(500).Produces<Flashsale>();
+        app.MapGet("/flashsales", (FinalPriceHandler handler) => handler.GetFlashSales()).Produces(200).Produces(404).Produces<List<Flashsale>>();
+        app.MapGet("/flashsale/{id}", (FinalPriceHandler handler, int id) => handler.GetFlashSale(id)).Produces(200).Produces(404).Produces<Flashsale>();
+        app.MapPost("/flashsale", (FinalPriceHandler handler, Flashsale flashsale) => handler.CreateFlashSale(flashsale)).Produces(200).Produces(500).Produces<Flashsale>();
+        app.MapPut("/flashsale/{id}", (FinalPriceHandler handler, Flashsale flashsale, int id) => handler.UpdateFlashSale(flashsale, id)).Produces(200).Produces(404).Produces(500).Produces<Flashsale>();
+        app.MapDelete("/flashsale/{id}", (FinalPriceHandler handler, int id) => handler.RemoveFlashSale(id)).Produces(200).Produces(404).Produces(500).Produces<Flashsale>();
     }
 }
