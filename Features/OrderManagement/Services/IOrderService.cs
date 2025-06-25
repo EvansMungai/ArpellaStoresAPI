30136f430fcf96443934d1b8fb3d5203acd95d71
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-06-25 23:57:38 +0300

    Fix Order management services and endpoints.

--- Features/OrderManagement/Services/IOrderService.cs
+++ Features/OrderManagement/Services/IOrderService.cs
@@ -5,8 +5,8 @@ namespace ArpellaStores.Features.OrderManagement.Services;
 public interface IOrderService
 {
     Task<IResult> GetOrders();
-    Task<IResult> GetOrder(string id);
-    Task<IResult> CreateOrder(Order category);
+    Task<IResult> GetOrder(string orderId);
+    Task<IResult> CreateOrder(Order order);
     //Task<IResult> UpdateOrderDetails(Order update, string id);
-    Task<IResult> RemoveOrder(string categoryId);
+    Task<IResult> RemoveOrder(string orderId);
 }
