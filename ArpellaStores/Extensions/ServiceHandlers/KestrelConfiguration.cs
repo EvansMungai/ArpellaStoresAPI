25fbc34d78da4b66476e46c0d9e0312b671c536e
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-06-27 01:26:12 +0300

    Check if app is actually using .env at runtime.

--- ArpellaStores/Extensions/ServiceHandlers/KestrelConfiguration.cs
+++ ArpellaStores/Extensions/ServiceHandlers/KestrelConfiguration.cs
@@ -8,13 +8,17 @@ public static class KestrelConfiguration
     public static void ConfigureCustomHttps(this KestrelServerOptions options)
     {
         var certPath = Environment.GetEnvironmentVariable("HTTPS_PFX_PATH");
-        var password = Environment.GetEnvironmentVariable("HTTPS_PFX_PASSWORD");
+        var certPwd = Environment.GetEnvironmentVariable("HTTPS_PFX_PASSWORD");
+
+        System.IO.File.WriteAllText("/app/kestrel-check.txt",
+    $"PFX_PATH: {(string.IsNullOrEmpty(certPath) ? "null" : certPath)}\n" +
+    $"PFX_PASSWORD: {(string.IsNullOrEmpty(certPwd) ? "null or empty" : "set")}");
 
         options.ListenAnyIP(8081, listenOptions =>
         {
             try
             {
-                var cert = new X509Certificate2(certPath, password, X509KeyStorageFlags.Exportable);
+                var cert = new X509Certificate2(certPath, certPwd, X509KeyStorageFlags.Exportable);
                 listenOptions.UseHttps(cert);
             }
             catch (Exception ex) { Console.WriteLine($"Failed to load certificate: {ex.Message}"); }
