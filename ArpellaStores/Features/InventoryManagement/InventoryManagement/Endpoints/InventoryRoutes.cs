444fe029861f75375436e7359b601bc6894fabaa
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-07-03 12:22:53 +0300

    feat(api): Add validation to inventory management.

--- ArpellaStores/Features/InventoryManagement/InventoryManagement/Endpoints/InventoryRoutes.cs
+++ ArpellaStores/Features/InventoryManagement/InventoryManagement/Endpoints/InventoryRoutes.cs
@@ -1,4 +1,5 @@
-﻿using ArpellaStores.Extensions.RouteHandlers;
+﻿using ArpellaStores.Extensions;
+using ArpellaStores.Extensions.RouteHandlers;
 using ArpellaStores.Features.InventoryManagement.Models;
 
 
@@ -15,9 +16,9 @@ public class InventoryRoutes : IRouteRegistrar
         var app = webApplication.MapGroup("").WithTags("Inventories");
         app.MapGet("/inventories", (InventoryHandler handler) => handler.GetInventories()).Produces(200).Produces(404).Produces<List<Inventory>>();
         app.MapGet("/inventory/{id}", (InventoryHandler handler ,string id) => handler.GetInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
-        app.MapPost("/inventory", (InventoryHandler handler, Inventory  inventory) => handler.CreateInventory(inventory)).Produces(200).Produces(404).Produces<Inventory>();
+        app.MapPost("/inventory", (InventoryHandler handler, Inventory inventory) => handler.CreateInventory(inventory)).Produces(200).Produces(404).Produces<Inventory>().AddEndpointFilter<ValidationEndpointFilter<Inventory>>();
         app.MapPost("/inventories", (InventoryHandler handler, IFormFile file) => handler.CreateInventories(file)).Produces(200).Produces(404).Produces<List<Inventory>>().DisableAntiforgery();
-        app.MapPut("/inventory/{id}", (InventoryHandler handler, Inventory inventory, string id) => handler.UpdateInventory(inventory, id)).Produces(200).Produces(404).Produces<Inventory>();
+        app.MapPut("/inventory/{id}", (InventoryHandler handler, Inventory inventory, string id) => handler.UpdateInventory(inventory, id)).Produces(200).Produces(404).Produces<Inventory>().AddEndpointFilter<ValidationEndpointFilter<Inventory>>();
         app.MapDelete("/inventory/{id}", (InventoryHandler handler, string id) => handler.RemoveInventory(id)).Produces(200).Produces(404).Produces<Inventory>();
         app.MapGet("/inventorylevels", (InventoryHandler handler) => handler.CheckInventoryLevels()).Produces(200).Produces(404).Produces<List<Inventory>>();
     }
--- ArpellaStores/Features/InventoryManagement/InventoryManagement/Models/Inventory.cs
+++ ArpellaStores/Features/InventoryManagement/InventoryManagement/Models/Inventory.cs
@@ -1,20 +1,29 @@
-﻿namespace ArpellaStores.Features.InventoryManagement.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArpellaStores.Features.InventoryManagement.Models;
 
 public partial class Inventory
 {
     public int InventoryId { get; set; }
+
+    [Required(ErrorMessage = "Product Id is required.")]
+    [StringLength(30, ErrorMessage =" Proudct id must be at most 30 characters.")]
     public string ProductId { get; set; }
 
+    [Required(ErrorMessage = "Stock quantity is required.")]
     public int? StockQuantity { get; set; }
 
+    [Required(ErrorMessage = "Stoc threshold is required.")]
     public int? StockThreshold { get; set; }
 
+    [Required(ErrorMessage = "Stock price is required.")]
     public decimal? StockPrice { get; set; }
 
     public DateTime? CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
 
+    [Required(ErrorMessage = "Supplier Id is required")]
     public int? SupplierId { get; set; }
 
     public virtual Supplier? Supplier { get; set; }
