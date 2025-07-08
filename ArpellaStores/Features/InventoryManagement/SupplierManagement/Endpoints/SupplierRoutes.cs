7f62cf5d7227a98f80ecf7cc1009e34affe8f28c
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-07-08 12:06:16 +0300

    feat(api): Add validation filter to Supplier management routes.

--- ArpellaStores/Features/InventoryManagement/SupplierManagement/Endpoints/SupplierRoutes.cs
+++ ArpellaStores/Features/InventoryManagement/SupplierManagement/Endpoints/SupplierRoutes.cs
@@ -1,4 +1,5 @@
-﻿using ArpellaStores.Extensions.RouteHandlers;
+﻿using ArpellaStores.Extensions;
+using ArpellaStores.Extensions.RouteHandlers;
 using ArpellaStores.Features.InventoryManagement.Models;
 
 namespace ArpellaStores.Features.InventoryManagement.Endpoints;
@@ -14,8 +15,8 @@ public class SupplierRoutes : IRouteRegistrar
         var app = webApplication.MapGroup("").WithTags("Suppliers");
         app.MapGet("/suppliers", (SupplierHandler handler) => handler.GetSuppliers()).Produces(200).Produces(404).Produces<List<Supplier>>();
         app.MapGet("/supplier/{id}", (SupplierHandler handler,int id) => handler.GetSupplier(id)).Produces(200).Produces(404).Produces<Supplier>();
-        app.MapPost("/supplier", (SupplierHandler handler, Supplier supplier) => handler.CreateSupplier(supplier)).Produces<Supplier>();
-        app.MapPut("/supplier/{id}", (SupplierHandler handler, Supplier supplier, int id) => handler.EditSupplierDetails(supplier, id)).Produces<Supplier>();
+        app.MapPost("/supplier", (SupplierHandler handler, Supplier supplier) => handler.CreateSupplier(supplier)).Produces<Supplier>().AddEndpointFilter<ValidationEndpointFilter<Supplier>>();
+        app.MapPut("/supplier/{id}", (SupplierHandler handler, Supplier supplier, int id) => handler.EditSupplierDetails(supplier, id)).Produces<Supplier>().AddEndpointFilter<ValidationEndpointFilter<Supplier>>();
         app.MapDelete("/supplier/{id}", (SupplierHandler handler, int id) => handler.RemoveSupplier(id)).Produces(200).Produces(404).Produces<Supplier>();
     }
 }
--- ArpellaStores/Features/InventoryManagement/SupplierManagement/Models/Supplier.cs
+++ ArpellaStores/Features/InventoryManagement/SupplierManagement/Models/Supplier.cs
@@ -1,11 +1,17 @@
-﻿namespace ArpellaStores.Features.InventoryManagement.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArpellaStores.Features.InventoryManagement.Models;
 
 public partial class Supplier
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Supplier Name is required.")]
+    [StringLength(50, ErrorMessage = " Supplier Name must be at most 30 characters.")]
     public string? SupplierName { get; set; }
 
+    [Required(ErrorMessage = "KRA pin is required.")]
+    [StringLength(30, ErrorMessage ="KRA pin must be at most 30 characters.")]
     public string? KraPin { get; set; }
 
     public virtual ICollection<Inventory> Inventories { get; set; } = new List<Inventory>();
