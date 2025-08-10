ecaaaf49a1748df9dd26b548b631728d14e4d518
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-08-10 18:36:48 +0300

    fix(api): Fix payment to go to correct till number.

--- ArpellaStores/Features/PaymentManagement/Common/MpesaConfig.cs
+++ ArpellaStores/Features/PaymentManagement/Common/MpesaConfig.cs
@@ -5,6 +5,7 @@ public class MpesaConfig
     public string ConsumerKey { get; set; }
     public string ConsumerSecret { get; set; }
     public string BusinessShortCode { get; set; }
+    public string TillNumber { get; set; }
     public string Passkey { get; set; }
     public string CallbackUri { get; set; }
     public string ConfirmationUri { get; set; }
