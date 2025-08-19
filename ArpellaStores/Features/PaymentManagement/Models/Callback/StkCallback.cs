cb6c23f7b7b40ea750a45ce33721e7bb331e088d
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-08-19 15:50:54 +0300

    fix(api): Fix callback model mismatch due to casing and naming conventions.

--- ArpellaStores/Features/PaymentManagement/Models/Callback/StkCallback.cs
+++ ArpellaStores/Features/PaymentManagement/Models/Callback/StkCallback.cs
@@ -1,10 +1,21 @@
-﻿namespace ArpellaStores.Features.PaymentManagement.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace ArpellaStores.Features.PaymentManagement.Models;
 
 public class StkCallback
 {
+    [JsonPropertyName("MerchantRequestID")]
     public string MerchantRequestID { get; set; }
+
+    [JsonPropertyName("CheckoutRequestID")]
     public string CheckoutRequestID { get; set; }
+
+    [JsonPropertyName("ResultCode")]
     public int ResultCode { get; set; }
+
+    [JsonPropertyName("ResultDesc")]
     public string ResultDesc { get; set; }
+
+    [JsonPropertyName("CallbackMetadata")]
     public CallbackMetadata CallbackMetadata { get; set; }
 }
