5bba66961912c7fccb6f5236e7a244e47d126e39
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-09-26 11:44:20 +0300

    feat(products): Return the show online field and set it to be updated on product details update.

--- ArpellaStores/Features/InventoryManagement/ProductManagement/Services/Repository/ProductRepository.cs
+++ ArpellaStores/Features/InventoryManagement/ProductManagement/Services/Repository/ProductRepository.cs
@@ -1,7 +1,6 @@
 ﻿using ArpellaStores.Data.Infrastructure;
 using ArpellaStores.Features.InventoryManagement.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Formats.Asn1;
 
 namespace ArpellaStores.Features.InventoryManagement.Services;
 
@@ -30,24 +29,24 @@ public class ProductRepository : IProductRepository
 
     public async Task<List<Product>> GetAllProductsAsync()
     {
-        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt }).AsNoTracking().ToListAsync();
+        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, ShowOnline = p.ShowOnline, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt }).AsNoTracking().ToListAsync();
     }
 
     public async Task<List<Product>> GetPagedProductsAsync(int pageNumber, int pageSize)
     {
-        return await _context.Products.Where(p => p.ShowOnline == true).Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt })
+        return await _context.Products.Where(p => p.ShowOnline == true).Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, ShowOnline = p.ShowOnline, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt })
             .Skip((pageNumber - 1) * pageSize).Take(pageSize)
             .AsNoTracking().ToListAsync();
     }
     public async Task<List<Product>> GetPagedPOSProductsAsync(int pageNumber, int pageSize)
     {
-        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt })
+        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, ShowOnline = p.ShowOnline, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt })
             .Skip((pageNumber - 1) * pageSize).Take(pageSize)
             .AsNoTracking().ToListAsync();
     }
     public async Task<Product?> GetProductByIdAsync(int id)
     {
-        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt }).AsNoTracking().SingleOrDefaultAsync(p => p.Id == id);
+        return await _context.Products.Select(p => new Product { Id = p.Id, InventoryId = p.InventoryId, Name = p.Name, Price = p.Price, PriceAfterDiscount = p.PriceAfterDiscount, Category = p.Category, PurchaseCap = p.PurchaseCap, Subcategory = p.Subcategory, Barcodes = p.Barcodes, DiscountQuantity = p.DiscountQuantity, ShowOnline = p.ShowOnline, CreatedAt = p.CreatedAt, UpdatedAt = p.UpdatedAt }).AsNoTracking().SingleOrDefaultAsync(p => p.Id == id);
     }
 
     public async Task RemoveProduct(int id)
@@ -72,6 +71,7 @@ public class ProductRepository : IProductRepository
         retrievedProduct.Barcodes = product.Barcodes;
         retrievedProduct.PurchaseCap = product.PurchaseCap;
         retrievedProduct.PriceAfterDiscount = product.PriceAfterDiscount;
+        retrievedProduct.ShowOnline = product.ShowOnline;
 
         _context.Products.Update(retrievedProduct);
         await _context.SaveChangesAsync();
